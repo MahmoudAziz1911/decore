a999d217ff298293b76e505d6acc39ba9c3d3a46
Mahmoud <ahmed709913@gmail.com>
2023-02-28 15:05:34 +0200

    modified url

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   env: {
-    BASE_URL: "http://localhost:3000",
+    BASE_URL: "https://vercel.com",
     MONGODB_URL: "mongodb+srv://khaled:khaled897@cluster0.xy7kahv.mongodb.net/fashion?retryWrites=true&w=majority",
   }
 }
